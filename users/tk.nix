f93a33966e58aae7dea95743af3d2eb2a7ffcae6
Litschi-zaphod <t.kratz@posteo.de>
2020-06-10 18:23:18 +0200

    users/tk.nix: added tk to input group

--- users/tk.nix
+++ users/tk.nix
@@ -3,7 +3,7 @@
   users.users.tk = {
     isNormalUser = true;
     uid = 1000;
-    extraGroups = [ "wheel" "audio" "video" "libvirtd" "dialout" "networkmanager" "docker" "mpd" "pulseaudio" "adbusers"];
+    extraGroups = [ "wheel" "audio" "video" "libvirtd" "dialout" "networkmanager" "docker" "mpd" "pulseaudio" "adbusers" "input" ];
     shell = pkgs.zsh;
     openssh.authorizedKeys.keys = (import ../nix-secret/secrets.nix).ssh;
     packages = with pkgs; [
