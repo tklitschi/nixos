0e9a4046472f3e13b84187c0c2f57d35b668ec3c
eddi <t.kratz@posteo.de>
2020-06-14 19:25:44 +0200

    modules/yubikey.nix: added yubikey-manager gui"

--- modules/yubikey.nix
+++ modules/yubikey.nix
@@ -5,7 +5,7 @@
   hardware.u2f.enable = true;
 
   environment.systemPackages = with pkgs; [
-    opensc pcsctools libu2f-host yubikey-personalization yubikey-manager
+    opensc pcsctools libu2f-host yubikey-personalization yubikey-manager yubikey-manager-qt
     (openvpn.override { pkcs11Support = true; })
   ];
   services.pcscd.enable = true;
