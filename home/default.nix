710e992ec543b5feab45f26991c3cea5245ed9f2
eddi <t.kratz@posteo.de>
2020-06-15 02:08:21 +0200

    home/default.nix: added gtk section and included dunst.nix

--- home/default.nix
+++ home/default.nix
@@ -1,4 +1,4 @@
-{ config, lib, ... }:
+{pkgs, config, lib, ... }:
 {
   imports = [
     <home-manager/nixos>
@@ -10,10 +10,57 @@
     services.network-manager-applet.enable = true;
     
     imports = [
-
+      ./programs/ssh.nix
       ./programs/vim.nix
-      ./services/i3.nix 
       ./programs/kitty.nix
+
+      ./services/i3.nix
+      ./services/dunst.nix 
     ];
+
+    gtk = {
+      enable = true;
+      font = {
+        package = pkgs.jetbrains-mono;
+        name = "JetBrainsMono 8";
+      };
+      theme = {
+        package = pkgs.nordic;
+        name = "Nordic";
+      };
+      iconTheme = {
+            name = "Qogir-dark";
+            package = pkgs.qogir-icon-theme;
+      };
+      gtk3.extraConfig = {
+        gtk-cursor-theme-size=24;
+        gtk-toolbar-style="GTK_TOOLBAR_BOTH_HORIZ";
+        gtk-toolbar-icon-size="GTK_ICON_SIZE_LARGE_TOOLBAR";
+        gtk-button-images=0;
+        gtk-menu-images=0;
+        gtk-enable-event-sounds=1;
+        gtk-enable-input-feedback-sounds=0;
+        gtk-xft-antialias=1;
+        gtk-xft-hinting=1;
+        gtk-xft-hintstyle="hintslight";
+        gtk-xft-rgba="rgb";
+        gtk-cursor-theme-name="Adwaita";
+        gtk-application-prefer-dark-theme=0;
+      };
+      gtk2.extraConfig = ''
+        gtk-cursor-theme-name="Adwaita"
+        gtk-cursor-theme-size=24
+        gtk-toolbar-style=GTK_TOOLBAR_BOTH_HORIZ
+        gtk-toolbar-icon-size=GTK_ICON_SIZE_LARGE_TOOLBAR
+        gtk-button-images=0
+        gtk-menu-images=0
+        gtk-enable-event-sounds=1
+        gtk-enable-input-feedback-sounds=0
+        gtk-xft-antialias=1
+        gtk-xft-hinting=1
+        gtk-xft-hintstyle="hintslight"
+        gtk-xft-rgba="rgb"
+      '';
+    };
   };
 }
