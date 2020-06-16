18e97b839e02baf05616cef77539e1e97f98da5d
eddi <t.kratz@posteo.de>
2020-06-17 00:26:39 +0200

    home/programs/zsh.nix: re-enabled zsh.nix config instead of /modules/zsh.nix

---
+++ home/programs/zsh.nix
@@ -0,0 +1,39 @@
+{config, pkgs, lib, ...}:
+{
+    programs.fzf = {
+        enable = true;
+        enableZshIntegration = true;
+    };
+    programs.zsh = {
+        enable = true;
+        enableCompletion = true;
+        enableAutosuggestions = true;
+        history = {
+            save = 100000;
+            size = 100000;
+        };
+
+
+        shellAliases = {
+            ls = "exa";
+        };
+        
+        oh-my-zsh = {
+            enable = true;
+            plugins = [ "git" "sudo" "colored-man-pages" ];
+        };
+
+        initExtra = ''
+            if [ -f "$HOME/.p10k.zsh" ]; then
+                source "$HOME/.p10k.zsh"
+            fi
+
+            function t {
+                cd $(mktemp -d $XDG_RUNTIME_DIR/$1.XXXX)
+            }
+            GITSTATUS_DAEMON=${pkgs.gitAndTools.gitstatus}/bin/gitstatusd
+            source ${pkgs.zsh-powerlevel10k}/share/zsh-powerlevel10k/powerlevel10k.zsh-theme
+            #ZSH_THEME=powerlevel9k
+        '';
+    };
+}
